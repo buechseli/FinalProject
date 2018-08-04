51b4f337cfa892446ea99c9224006448e697fde2
Grace Chan <gkchan@qualcomm.com>
2018-08-04 14:17:19 -0700

    adding presentation and bokeh visualiation

--- blah2.py
+++ blah2.py
@@ -66,10 +66,10 @@ country_names
 from bokeh.plotting import figure, output_file,show
 from bokeh.models.widgets import Select, Tabs
 from bokeh.io import show, curdoc
-from bokeh.layouts import column, row, WidgetBox
+from bokeh.layouts import column, row, WidgetBox, gridplot
 
 
-# In[48]:
+# In[7]:
 
 
 #grabbing lists from "CSV_analysis.ipynb" 
@@ -90,7 +90,70 @@ arrivals_list = ['FRANCE',
  'POLAND']
 
 
-# In[53]:
+# In[8]:
+
+
+#grabbing lists from "CSV_analysis.ipynb" 
+departures_list = ['HONG KONG, CHINA',
+ 'UNITED STATES OF AMERICA',
+ 'GERMANY',
+ 'UNITED KINGDOM',
+ 'POLAND',
+ 'CHINA',
+ 'RUSSIAN FEDERATION',
+ 'ITALY',
+ 'CANADA',
+ 'FRANCE',
+ 'HUNGARY',
+ 'JAPAN',
+ 'NETHERLANDS',
+ 'UKRAINE',
+ 'MALAYSIA']
+
+
+# In[9]:
+
+
+#grabbing lists from "CSV_analysis.ipynb" 
+expenditure_in_list = ['UNITED STATES OF AMERICA',
+ 'FRANCE',
+ 'GERMANY',
+ 'UNITED KINGDOM',
+ 'ITALY',
+ 'AUSTRALIA',
+ 'THAILAND',
+ 'SPAIN',
+ 'HONG KONG, CHINA',
+ 'MACAO, CHINA',
+ 'CANADA',
+ 'TURKEY',
+ 'SWITZERLAND',
+ 'GREECE',
+ 'MEXICO']
+
+
+# In[10]:
+
+
+#grabbing lists from "CSV_analysis.ipynb" 
+expenditure_out_list =['UNITED STATES OF AMERICA',
+ 'GERMANY',
+ 'UNITED KINGDOM',
+ 'JAPAN',
+ 'FRANCE',
+ 'CANADA',
+ 'RUSSIAN FEDERATION',
+ 'AUSTRALIA',
+ 'KOREA, REPUBLIC OF',
+ 'ITALY',
+ 'SWITZERLAND',
+ 'BRAZIL',
+ 'NORWAY',
+ 'BELGIUM',
+ 'SAUDI ARABIA']
+
+
+# In[11]:
 
 
 colors = ['pink',
@@ -110,7 +173,7 @@ colors = ['pink',
           'cadetblue']
 
 
-# In[54]:
+# In[29]:
 
 
 #Function to make the plot
@@ -185,8 +248,8 @@ def select_country_handler(attr, old, new):
     data_source_global.data.update(new_cds.data)
     return
 
-def make_fifteen_chart_cds(arrivals_list):
-    plot = figure(title="MAKE FIFTEEN LINE ('000s)")
+def make_arrival_chart_cds(arrivals_list):
+    plot = figure(title="Top 15 Countries by Total Arrivals ('000s)")
     for i in range(len(arrivals_list)):
         country_cds = get_cds(arrivals_list[i])
         plot.line(x='years', 
@@ -203,89 +266,79 @@ def make_fifteen_chart_cds(arrivals_list):
     plot.xaxis.axis_label = "Year"
     return plot 
 
-
-data_source_global = get_cds(country_names[0])
-plot_global        = make_chart_cds(data_source_global)
-plot_global_2      = make_chart_exp_cds(data_source_global)
-plot_15_arrival    = make_fifteen_chart_cds(arrivals_list)
-
-select_country = Select(title="Country:", 
-                        value=country_names[0],
-                        options=country_names)
-
-select_country.on_change("value", select_country_handler)
-layout = row(select_country, plot_global, plot_global_2, plot_15_arrival)
-tab = Panel(child=layout, title='Country')
-tabs = Tabs(tabs=[tab])
-
-curdoc().add_root(tabs)
-
-
-# In[38]:
-
-
-#grabbing lists from "CSV_analysis.ipynb" 
-arrivals_list = ['FRANCE',
- 'UNITED STATES OF AMERICA',
- 'SPAIN',
- 'CHINA',
- 'ITALY',
- 'HONG KONG, CHINA',
- 'RUSSIAN FEDERATION',
- 'UNITED KINGDOM',
- 'GERMANY',
- 'MEXICO',
- 'TURKEY',
- 'AUSTRIA',
- 'CANADA',
- 'MALAYSIA',
- 'POLAND']
-
-
-# In[44]:
-
-
-def make_fifteen_chart_cds(arrivals_list):
-    plot = figure(title="MAKE FIFTEEN LINE ('000s)")
-    for country_name in arrivals_list:
-        country_cds = get_cds(country_name)
+def make_departure_chart_cds(departures_list):
+    plot = figure(title="Top 15 Countries by Total Departures ('000s)")
+    for i in range(len(departures_list)):
+        country_cds = get_cds(departures_list[i])
         plot.line(x='years', 
-                  y='arrivals',
+                  y='departures',
                   source=country_cds, 
-                  line_color='green', 
+                  line_color=colors[i], 
                   line_width=2,
-                  muted_color = 'green',
+                  muted_color = colors[i],
                   muted_alpha = 0.2,
-                  legend = country_name)
+                  legend = departures_list[i])
     plot.legend.location = 'top_left'
     plot.legend.click_policy = 'mute'
     plot.yaxis.axis_label = "Tourists ('000s)"
     plot.xaxis.axis_label = "Year"
     return plot 
 
+def make_expenditure_in_chart_cds(expenditure_in_list):
+    plot = figure(title="Top 15 Countries by Tourism Expenditure In Country - US$ M")
+    for i in range(len(expenditure_in_list)):
+        country_cds = get_cds(expenditure_in_list[i])
+        plot.line(x='years', 
+                  y='expenditures_in',
+                  source=country_cds, 
+                  line_color=colors[i], 
+                  line_width=2,
+                  muted_color = colors[i],
+                  muted_alpha = 0.2,
+                  legend = expenditure_in_list[i])
+    plot.legend.location = 'top_left'
+    plot.legend.click_policy = 'mute'
+    plot.yaxis.axis_label = "US$ - M"
+    plot.xaxis.axis_label = "Year"
+    return plot
 
-# In[40]:
+def make_expenditure_out_chart_cds(expenditure_out_list):
+    plot = figure(title="Top 15 Countries by Tourism Expenditure In Other Country - US$ M")
+    for i in range(len(expenditure_out_list)):
+        country_cds = get_cds(expenditure_out_list[i])
+        plot.line(x='years', 
+                  y='expenditures_out',
+                  source=country_cds, 
+                  line_color=colors[i], 
+                  line_width=2,
+                  muted_color = colors[i],
+                  muted_alpha = 0.2,
+                  legend = expenditure_out_list[i])
+    plot.legend.location = 'top_left'
+    plot.legend.click_policy = 'mute'
+    plot.yaxis.axis_label = "US$ - M"
+    plot.xaxis.axis_label = "Year"
+    return plot 
 
+data_source_global = get_cds(country_names[0])
+plot_global        = make_chart_cds(data_source_global)
+plot_global_2      = make_chart_exp_cds(data_source_global)
+plot_15_arrival    = make_arrival_chart_cds(arrivals_list)
+plot_15_departure  = make_departure_chart_cds(departures_list)
+plot_15_expend_in  = make_expenditure_in_chart_cds(expenditure_in_list)
+plot_15_expend_out = make_expenditure_out_chart_cds(expenditure_out_list)
 
-print(big_list)
+select_country = Select(title="Country:", 
+                        value=country_names[0],
+                        options=country_names)
 
+select_country.on_change("value", select_country_handler)
+layout = gridplot([select_country], [plot_global, plot_global_2],
+                  [plot_15_arrival, None], [plot_15_departure, None],
+                  [plot_15_expend_in, None],[plot_15_expend_out, None])
 
-# In[ ]:
+tab = Panel(child=layout, title='Country')
+tabs = Tabs(tabs=[tab])
 
-
-departures_list = ['HONG KONG, CHINA',
- 'UNITED STATES OF AMERICA',
- 'GERMANY',
- 'UNITED KINGDOM',
- 'POLAND',
- 'CHINA',
- 'RUSSIAN FEDERATION',
- 'ITALY',
- 'CANADA',
- 'FRANCE',
- 'HUNGARY',
- 'JAPAN',
- 'NETHERLANDS',
- 'UKRAINE',
- 'MALAYSIA']
+curdoc().add_root(tabs)
 
